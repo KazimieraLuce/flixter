fad27c20bc48bba7ad18e19c098812a620d3393d
KazimieraLuce <kazimiera.luce@aya.yale.edu>
2019-03-12 00:42:57 +0000

    restrict user access to enrolled courses

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -10,12 +10,17 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
 
   def require_authorized_for_current_lesson
-    if current_user != current_user.enrolled_in?(current_lesson.section.course)
+    if current_user != current_user.enrolled_in?()
+    #if current_user.enrolled_in?(current_lesson.section.course) == false
       redirect_to course_path, alert: "You must enroll in the course to view this page"
     end
   end
 
   def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
+    current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def lesson_params #consider deleting, as I don't think it changed anything
+    params.require(:lesson).permit(:title, :subtitle, :video)
   end
 end
