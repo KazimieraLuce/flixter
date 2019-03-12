8bb0b7182e34f7686d1a049ee7dffa7c62b07082
KazimieraLuce <kazimiera.luce@aya.yale.edu>
2019-03-12 19:05:17 +0000

    redirect to course id

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,18 +8,24 @@ class LessonsController < ApplicationController
   private
 
   helper_method :current_lesson
+  helper_method :current_course #didn't help, consider deleting
 
   def require_authorized_for_current_lesson
-    if current_user != current_user.enrolled_in?(current_lesson.section.course)
-    #if current_user.enrolled_in?(current_lesson.section.course) == false
+    # course = current_lesson.section.course(:course_id)
+    if current_user.enrolled_in?(current_lesson.section.course) == false
+      #doesn't work with just course, current_course or @course as enrolled_in? argument
+    #tried if current_user != current_user.enrolled_in? and as expected, did not work
       redirect_to course_path, alert: "You must enroll in the course to view this page"
+      #tried course_path(course) and course_path(@course) and course_path(current_course)
     end
   end
 
   def current_lesson
     current_lesson ||= Lesson.find(params[:id])
   end
-
+  def current_course #didn't help, consider deleting   
+    current_course ||= Course.find(params[:course_id])
+  end
   def lesson_params #consider deleting, as I don't think it changed anything
     params.require(:lesson).permit(:title, :subtitle, :video)
   end
